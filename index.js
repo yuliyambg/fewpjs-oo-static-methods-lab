b2b37cedea0b2b9865e89e94e140398315d0cd21
yuliyambg <yuliyambg@gmail.com>
2021-02-24 20:28:30 -0500

    commit tree 0df499ece8efe92b84245f9a98f148f792918661

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+    static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize( string ) {
+        return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+    }
+
+    static titleize(string) {
+        const dontCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        const newArr = [];
+        const wordArray = string.split(' ');
+        for (let i = 0; i < wordArray.length; i++) {
+            if (!dontCapitalize.includes(wordArray[i])) {
+                newArr.push(this.capitalize(wordArray[i]))
+            } else {
+                newArr.push(wordArray[i])
+            }
+        }
+        return Formatter.capitalize(newArr.join(' '))
+    }
 }
